aeda26c5fdd25e06a791b2f194415ab8e170c781
anshu5718 <123818000+anshu5718@users.noreply.github.com>
2025-08-13 23:26:51 +0545

    Initial commit: Added Django Vehicle Reservation System project

---
+++ Swari_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SwariAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Swari_app'
---
+++ Swari_app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'Sawari_app/home.html')
+# Create your views here.
